8c2e5c307e8eb1d948da7b3edfea8d3ddb347d79
John Rice <john.rice@revature.com>
2020-10-16 17:08:45 -0400

    - added notRecognized exception catcher when non-programmed commands are used

--- src/main/scala/csvreadwrite/Cli.scala
+++ src/main/scala/csvreadwrite/Cli.scala
@@ -2,22 +2,23 @@ package csvreadwrite
 import scala.io.StdIn
 import scala.util.matching.Regex
 import java.io.FileNotFoundException
+
 import com.mongodb.client.model.Filters
-import org.mongodb.scala.model.Filters._
-import csvreadwrite.DBDriver.collection
+import org.mongodb.scala.model.Filters.{exists, _}
+import csvreadwrite.DBDriver.{collection, db}
+import org.mongodb.scala.{Document, model}
 
 
 
 class Cli {
 
-    val commandArgPattern : Regex = "(\\w+)\\s*(.*)".r
+    val userInputFormat : Regex = "(\\w+)\\s*(.*)".r
 
     def printWelcome(): Unit = {
-        for (repeat <- 1 to 6) {
+        for (repeat <- 1 to 8) {
             println("Welcome to the CSV Parser / Reader. This CSV Parser/Reader can also connect to MongoDB and store" +
                 " your contacts in the database!")
-            println("")
-            Thread.sleep(900)
+            Thread.sleep(300)
         }
     }
 
@@ -29,6 +30,7 @@ class Cli {
         println("view: view DB contacts")
         println("remove : remove specific contacts or all contacts from DB")
         println("exit : exit the application")
+
 
        /** FEATURES BELOW THAT HAVE YET TO BE IMPLEMENTED
 
@@ -47,7 +49,7 @@ class Cli {
 
             //user input: upload CSV file to DB
             StdIn.readLine() match{
-                case commandArgPattern(cmd, arg) if cmd.equals("upload") =>
+                case userInputFormat(cmd, arg) if cmd.equals("upload") =>
                     try {
                         CSVParser.parseCSV(arg)
                     } catch {
@@ -55,62 +57,65 @@ class Cli {
                     }
 
                 //user input: view contacts in DB
-                case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("view") =>
+                case userInputFormat(cmd, arg) if cmd.equalsIgnoreCase("view") =>
                     DBDriver.printResults(collection.find())
 
                 //user input: add a contact to DB
-                case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("add") => {
-                    println("What is the contact's name? ")
+                case userInputFormat(cmd, arg) if cmd.equalsIgnoreCase("add") => {
+                        println("What is the contact's name? ")
                     val nameInput = StdIn.readLine()
-                    println("What is the contact's phone number? (enter only numbers) ")
+                        println("What is the contact's phone number? (enter only numbers, no country code) ")
                     val phoneInput = StdIn.readLine()
-                    println("Are they friends, family, or co-workers? ")
+                        println("Are they friends, family, or co-workers? Enter one.")
                     val groupInput = StdIn.readLine()
-                    println("You're adding the '" +nameInput + "' with the phone number of '"
-                    + phoneInput + "' to the contact group '" +groupInput + "'. Is this correct? Yes or no.")
+                        println("You're adding the '" +nameInput + "' with the phone number of '"
+                    + phoneInput + "' to the contact group '" +groupInput + "'. Is this correct? Type 'yes' or 'no'.")
                     StdIn.readLine() match {
                         case "yes" => DBDriver.printResults(collection.insertOne(Contact(nameInput, phoneInput, groupInput)))
                             println("Your contact "+nameInput+ " has been successfully added to the database!")
                             println("")
                         case "no" => println("No new contacts were added. Please try again.")
                             println("")
+                        case notRecognized => println(s"$notRecognized is an invalid answer! Try again typing 'yes' or 'no'.")
                     }
 
                 }
 
                 //user input: remove one or all contacts from DB
-                case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("remove") => {
-                    println("Do you want to delete all contacts or a specific one? Type All or Specific.")
+                case userInputFormat(cmd, arg) if cmd.equalsIgnoreCase("remove") => {
+                    println("Do you want to delete all contacts or a specific one? Type 'all' or 'one'.")
                     StdIn.readLine() match {
                         case "all" =>
-                    println("Are you sure you want to delete all contacts? Yes or No.")
-                     StdIn.readLine() match {
-                        case "yes" => DBDriver.printResults(collection.deleteMany(Filters.exists("name")))
-                            println("ALL CONTACTS HAVE BEEN DELETED FROM YOUR DATABASE!")
-                            println("")
-                        case "no" => println("No contacts deleted.")
-                             println("")
-                    }
-                        case "specific" => println("What is the name of the contact you'd like to remove?")
+                            println("Are you sure you want to delete all contacts? Type 'yes' or 'no'.")
+                            StdIn.readLine() match {
+                                case "yes" => DBDriver.printResults(collection.deleteMany(Filters.exists("name")))
+                                    println("ALL CONTACTS HAVE BEEN DELETED FROM YOUR DATABASE!")
+                                    println("")
+                                case "no" => println("No contacts deleted.")
+                                    println("")
+                                case notRecognized => println(s"$notRecognized is an invalid answer! Try again typing 'yes' or 'no'.")
+                            }
+                        case "one" => println("What is the name of the contact you'd like to remove?")
                             val delContactName = StdIn.readLine()
                             DBDriver.printResults(collection.find(equal("name", delContactName)))
                             println("^^^ FOUND CONTACT ^^^")
                             println("Is this the contact you want to delete above?")
                             StdIn.readLine() match {
                                 case "yes" => DBDriver.printResults(collection.deleteOne(equal("name", delContactName)))
-                                    println("You successfully deleted '" +delContactName+ "' from your contacts " +
-                                        "database!")
-                                    println("")
+                                            println("You successfully deleted '" + delContactName + "' from your contacts " +
+                                                        "database!")
+                                            println("")
                                 case "no" => println("No specific contact has been deleted.")
-                                    println("")
-                            }
-
+                                            println("")
+                                case notRecognized => println(s"$notRecognized is an invalid answer! Try again typing 'yes' or 'no'.")
+                                            }
+                                    }
                     }
-                }
+
                 //user input: exit program
-                case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("exit") =>
+                case userInputFormat(cmd, arg) if cmd.equalsIgnoreCase("exit") =>
                     userMenuLoop = false
-                case notRecognized => println(s"$notRecognized not a recognized command!")
+                //case notRecognized => println(s"$notRecognized not a recognized command!")
             }
         }
 
